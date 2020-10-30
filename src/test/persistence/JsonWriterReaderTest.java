7c6a70d0e4b9b7f056292276e97e0c3af511e96f
SHAFQUAT UL BARI <sbarry11@students.cs.ubc.ca>
2020-10-31 02:25:55 +0600

    Fixed Test

--- src/test/persistence/JsonWriterReaderTest.java
+++ src/test/persistence/JsonWriterReaderTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class JsonWriterTest extends JsonTest{
+public class JsonWriterReaderTest extends JsonTest{
 
     @Test
     void testWriterInvalidFile() {
