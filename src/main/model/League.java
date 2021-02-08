9ba858f7dc6f5bb1c477991d6b2c8ecde1806238
SHAFQUAT UL BARI <sbarry11@students.cs.ubc.ca>
2021-02-08 19:13:04 +0600

    Phase '4'

--- src/main/model/League.java
+++ src/main/model/League.java
@@ -11,11 +11,11 @@ public class League implements Writable {
     //This class represents information of football League and behaviour and attributes associated with it.
     private List<Team> teams;
 
-
     //EFFECTS: constructs a League with an empty ArrayList.
     public League() {
         teams = new ArrayList<>();
     }
+
 
     //MODIFIES: this
     //EFFECTS: adds a team to the arraylist.
